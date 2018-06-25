88989414f56ff0084df0996c38284f6dd5560ea2
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-06-25 15:57:34 +0000

    rm underscore-contrib

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var _ = require('lodash');
 var async = require('async');
 
 var underscore = require('underscore');
-var underscoreContrib = require('underscore-contrib');
 
 var arrayFilter = require('array-filter');
 
@@ -75,18 +74,6 @@ async.timesSeries(
 
     (function() {
       var array = newArray();
-      var option = function(value) { return value.number === 1 };
-      var list = underscoreContrib.iterators.List(array);
-      suite.add('underscore-contrib@0.3.0 filter function', function() {
-        var value, values = [];
-        while (value = underscoreContrib.iterators.filter(list, option)) {
-          values.push(value);
-        }
-      });
-    })();
-
-    (function() {
-      var array = newArray();
       var option = function(value) { return value.number; };
       suite.add('lodash@4.17.10 map function', function() {
         _.map(array, option);
@@ -124,18 +111,6 @@ async.timesSeries(
       var option = function(value) { return value.number; };
       suite.add('underscore@1.9.1 map function', function() {
         underscore.map(array, option);
-      });
-    })();
-
-    (function() {
-      var array = newArray();
-      var option = function(value) { return value.number; };
-      var list = underscoreContrib.iterators.List(array);
-      suite.add('underscore-contrib@0.3.0 map function', function() {
-        var value, values = [];
-        while (value = underscoreContrib.iterators.map(list, option)) {
-          values.push(value);
-        }
       });
     })();
 
@@ -189,15 +164,6 @@ async.timesSeries(
       var option = function(value) { return value.number === 1 };
       suite.add('underscore@1.9.1 find function', function() {
         underscore.find(array, option);
-      });
-    })();
-
-    (function() {
-      var array = newArray();
-      var option = function(value) { return value.number === 1 };
-      var list = underscoreContrib.iterators.List(array);
-      suite.add('underscore-contrib@0.3.0 find function', function() {
-        underscoreContrib.iterators.find(list, option);
       });
     })();
 
